deeefd61e759bdf59efd1c92b75cf474dce91bdb
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2024-12-20 21:44:05 +0300

    add tests 2

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-import unittest
-
 def f(n): 
     sum = 0 
     for i in range(0,(n * 2)+1, 2): 
         sum += i 
     return sum
 
-print(f(5))
+def test_sum_fun1(self):
+    assert f(5) == 30
 
-class TestAddFunction(unittest.TestCase):
-    def test_sum_fun1(self):
-        self.assertEqual(f(5), 30)
+def test_sum_fun2(self):
+    assert f(10) == 110
 
-    def test_sum_fun2(self):
-        self.assertEqual(f(10), 110)
-
-    def test_sum_fun3(self):
-        self.assertEqual(f(15), 240)
-
-if __name__ == '__main__':
-    unittest.main()
+def test_sum_fun3(self):
+    assert f(15) == 240
